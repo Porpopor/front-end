e1c74cf645779a2aeaa4532e1489fe326023647b
multifriend <55123111+multifriend@users.noreply.github.com>
2022-03-01 18:23:37 +0700

    edit register

--- src/app/user-page/register/register.component.ts
+++ src/app/user-page/register/register.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
@@ -11,41 +11,71 @@ import { environment } from 'src/environments/environment';
 })
 export class RegisterComponent implements OnInit {
 
-  email ="";
+  registerForm = new FormGroup({
 
-  firstName ="";
+    emailForm: new FormControl('', [Validators.required, Validators.email]),
+    firstnameForm: new FormControl('', Validators.required),
+    lastnameForm: new FormControl('', Validators.required),
+    passwordForm: new FormControl('', Validators.required),
+    sexForm: new FormControl('', Validators.required),
+    phoneForm: new FormControl('', Validators.required)
 
-  lastName ="";
+  })
 
-  passWord ="";
 
-  emailForm = new FormControl(null,[Validators.required]);
-
-  firstnameForm = new FormControl(null,Validators.required);
 
   constructor(
-    private httpClient:HttpClient,
-    private router:Router
+    private httpClient: HttpClient,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
   }
+  get email() {
+    return this.registerForm.get('emailForm');
+  }
+  get firstname() {
+    return this.registerForm.get('firstnameForm');
+  }
+  get lastname() {
+    return this.registerForm.get('lastnameForm');
+  }
+  get password() {
+    return this.registerForm.get('passwordForm');
+  }
+  get sex() {
+    return this.registerForm.get('sexForm');
+  }
+  get phone() {
+    return this.registerForm.get('phoneForm');
+  }
 
-  sendit(data:any){
+
+  sendit(data: any) {
     // console.log("Valueasd",data);
     // this.onLogin2();
   }
 
-  // onLogin2() {
-  //   this.httpClient.post(`${environment.API_URL}/user-profile/save`, { userName: this.username, passWord: this.passWord })
-  //     .subscribe((res: any) => {
-  //       res.userName = this.username;
-  //       res.passWord = this.passWord;
-  //       console.log(res);
-  //       this.router.navigate(['/login']);
-  //       // this.cookie.put('token', res.data.token)
-  //       // this.router.navigate(["/home"])
-  //     })
-  // }
+  onLogin() {
+    console.log(this.registerForm.value);
+    this.httpClient.post(`${environment.API_URL}/user/register`,
+      {
+        email: this.email?.value,
+        passWord: this.password?.value,
+        firstName: this.firstname?.value,
+        lastName: this.lastname?.value,
+        sex: this.sex?.value,
+        phone: this.phone?.value
+
+      })
+      .subscribe((res: any) => {
+        console.log(res);
+        this.router.navigate(['/login']);
+        // this.cookie.put('token', res.data.token)
+        // this.router.navigate(["/home"])
+      },(error: any) => {
+          console.log(error.error.message);
+      })
+  }
 
 }
